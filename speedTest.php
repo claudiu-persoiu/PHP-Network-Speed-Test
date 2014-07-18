20123247379c8983080f49e6561eebd28da65f80
Claudiu <claudiu@claudiupersoiu.ro>
2014-07-18 12:38:56 +0300

    take into consideration microtime to make data more relevant for somaller files

--- speedTest.php
+++ speedTest.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Test started...' . PHP_EOL;
-$location = isset($argv[1]) ? $argv[1] : 'http://download.thinkbroadband.com/50MB.zip';
+$location = isset($argv[1]) ? $argv[1] : 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js';
 
 $handle = fopen($location, 'r');
 
@@ -9,21 +9,21 @@ if(!$handle) {
      die(1);
 }
 
-$timeStart = time();
+$timeStart = microtime(true);
 $packSize = 0;
 while(($buffer = fgets($handle, 4096)) !== false) {
      $packSize += strlen($buffer);
 }
 
-$timeEnd = time();
+$timeEnd = microtime(true);
 fclose($handle);
 
-$diff = $timeEnd - $timeStart;
+$diff = round($timeEnd - $timeStart, 4);
 
 if($diff == 0) { 
-     $diff = 1; // prevent division by 0
+     $diff = 0.0001; // prevent division by 0
 }
 
 $packSize = ($packSize / (1024 * 1024)) * 8;
-echo round($packSize, 2) . 'Mb in ' . $diff . ' seconds, ';
+echo round($packSize, 2) . 'Mb in ' . round($diff, 2) . ' seconds, ';
 echo round($packSize / $diff, 2) . 'Mb/s' . PHP_EOL;
